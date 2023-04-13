fad641d209f0e7e4cb3177d93f4fbbfd4d1832b4
akbar <jamalakbaralam@gmail.com>
2023-04-13 15:08:49 +0700

    give update next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = (process.env.NODE_ENV || 'production') === 'production'
-
 const nextConfig = {
   reactStrictMode: true,
-  exportPathMap: () => ({
-    '/': { page: '/' },
-  }),
-  assetPrefix: isProd ? '/bahana' : '',
 }
 
 module.exports = nextConfig
