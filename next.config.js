36bcff3727c0141e4e82ae8bbc3d37b583557746
akbar <jamalakbaralam@gmail.com>
2023-04-14 13:22:32 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  crossOrigin: 'anonymous'
+// const nextConfig = {
+//   reactStrictMode: true,
+//   crossOrigin: 'anonymous'
+// }
+
+// module.exports = nextConfig
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
 }
 
-module.exports = nextConfig
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}
+
