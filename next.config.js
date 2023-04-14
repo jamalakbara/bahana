1cda06219d133715994137eac1a0805b4fa7aafb
akbar <jamalakbaralam@gmail.com>
2023-04-14 13:26:52 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -6,18 +6,9 @@
 
 // module.exports = nextConfig
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
+const repo = 'bahana'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
 
 module.exports = {
   assetPrefix: assetPrefix,
